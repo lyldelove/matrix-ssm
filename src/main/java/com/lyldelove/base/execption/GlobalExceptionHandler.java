a2d93b83072f262dc5cab14d6f9e00b33d45c5cd
lyldelove <932563352@qq.com>
2020-05-07 07:16:38 +0800

    update 2020/05/07 update

--- src/main/java/com/lyldelove/base/execption/GlobalExceptionHandler.java
+++ src/main/java/com/lyldelove/base/execption/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import org.apache.shiro.authc.AuthenticationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 /**
